df305cd322a2a0d5f0cd8d53b8ddaa44b0f8ff6b
luismi4157 <10932238@uvu.edu>
2025-02-23 19:04:08 -0700

    .

--- GameStore.Api/GameStore.Api/Program.cs
+++ GameStore.Api/GameStore.Api/Program.cs
@@ -2,6 +2,7 @@ using GameStore.Api.Dtos;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+const string GateGameEndpointName = "GetGame";
 app.UseHttpsRedirection();
 
 List<GameDto> games = [
@@ -29,7 +30,56 @@ List<GameDto> games = [
 app.MapGet("/games", () => games);
 
 // GET /games/1
-app.MapGet("/games/{id}", (int id) => games.FirstOrDefault(g => g.Id == id));
+app.MapGet("/games/{id}", (int id) => 
+{
+    GameDto? game = games.Find(g => g.Id == id);
+
+    return game is null ? Results.NotFound() : Results.Ok(game);
+})
+.WithName(GateGameEndpointName);
+
+// POST /games
+app.MapPost("/games", (CreateGameDto newGame) => 
+{
+    var game = new GameDto(
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate);
+
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GateGameEndpointName, new { id = game.Id }, game);
+});
+
+// PUT /games/1
+app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) => 
+{
+    var index = games.FindIndex(g => g.Id == id);
+
+    if (index == -1)
+    {
+        return Results.NotFound();
+    }
+
+    games[index] = new GameDto(
+        id,
+        updatedGame.Name,
+        updatedGame.Genre,
+        updatedGame.Price,
+        updatedGame.ReleaseDate);
+
+    return Results.NoContent();
+});
+
+// DELETE /games/1
+app.MapDelete("/games/{id}", (int id) => 
+{
+    games.RemoveAll(games => games.Id == id);
+
+    return Results.NoContent();
+});
 
 app.Run();
 
